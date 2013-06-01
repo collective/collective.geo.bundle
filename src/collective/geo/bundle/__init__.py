5ede35dc70b3a966d25d5c10d7498daa88aaca7a
Giorgio Borelli <casaborelli@gmail.com>
2013-06-01 18:23:00 +0200

    hide c.z3cform packages

--- src/collective/geo/bundle/__init__.py
+++ src/collective/geo/bundle/__init__.py
@@ -3,6 +3,8 @@ DEPENDENCIES = [
     u'collective.geo.settings',
     u'collective.geo.openlayers',
     u'collective.geo.mapwidget',
+    u'collective.z3cform.mapwidget',
+    u'collective.z3cform.colorpicker',
     u'collective.geo.contentlocations',
     u'collective.geo.kml',
 ]
